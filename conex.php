5ba5608a94785de28502701f8dabe01550e16823
ANAbless <46084608+ANAbless@users.noreply.github.com>
2022-11-16 17:58:02 +0000

    create new file

---
+++ conex.php
@@ -0,0 +1,31 @@
+<?php
+
+class Database
+{
+    private static $servername = "localhost";
+    private static $username = "root";
+    private static $password = "";
+    private static $db = "funday";
+
+    private static $conn = null;
+
+    public static function connect()
+    {
+        try {
+    self::$conn = new PDO("mysql:host=" .self::$servername .";dbname=". self::$db , self::$username, self::$password);
+        // set the PDO error mode to exception
+        // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo json_encode(["Connected successfully"]) ;
+        }
+        catch(PDOException $e)
+        {
+            die($e->getMessage());
+        }
+        return self::$conn;
+    }
+    public static function disconnect()
+    {
+        self::$conn=null;
+    }
+}
+?>
